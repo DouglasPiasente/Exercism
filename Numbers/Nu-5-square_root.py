9fdfc8a964a30586f5e96280dc4c86e659fd93b5
Douglas Piasente <66640847+DouglasPiasente@users.noreply.github.com>
2023-11-01 23:03:49 -0300

    numbers 5 and dict 1 added

---
+++ Numbers/Nu-5-square_root.py
@@ -0,0 +1,10 @@
+""" 
+Given a natural radicand, return its square root.
+
+Note that the term "radicand" refers to the number for which the root is to be determined. That is, it is the number under the root symbol.
+
+Recall also that natural numbers are positive real whole numbers (i.e. 1, 2, 3 and up).
+"""
+
+def square_root(number):
+    return number**(1/2)
